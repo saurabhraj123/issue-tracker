1e53c59454c048c2a3bab7612b817ea312eba002
saurabhraj123 <me4saurabh4u@gmail.com>
2024-03-10 23:18:29 +0530

    Impove the loading experience

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,13 +1,1 @@
-import { Box } from "@radix-ui/themes";
-import { Skeleton } from "@/app/components";
-
-const NewIssuePageLoading = () => {
-  return (
-    <Box className="max-w-xl">
-      <Skeleton />
-      <Skeleton height="20rem" />
-    </Box>
-  );
-};
-
-export default NewIssuePageLoading;
+export { default } from "../_components/IssueFormSkeleton";
