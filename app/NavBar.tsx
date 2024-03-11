d710268a59f9beca33a3408dca34ae109c33e059
saurabhraj123 <me4saurabh4u@gmail.com>
2024-03-11 22:06:28 +0530

    Implement authentication

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
 import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const links = [
@@ -12,6 +14,7 @@ const NavBar = () => {
   ];
 
   const currentPath = usePathname();
+  const { data: session, status } = useSession();
 
   return (
     <nav className="flex items-center space-x-6 border-b px-5 mb-5 h-14">
@@ -32,6 +35,15 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Logout</Link>
+        )}
+
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
+      </Box>
     </nav>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Container, Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -21,13 +22,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="violet">
-          <NavBar />
+        <AuthProvider>
+          <Theme accentColor="violet">
+            <NavBar />
 
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
