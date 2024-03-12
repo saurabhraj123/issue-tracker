7e08af5d3e8eed2f5c02328f3382711b9d772d8f
saurabhraj123 <me4saurabh4u@gmail.com>
2024-03-13 00:38:24 +0530

    Add filter issues dropdown

---
+++ app/issues/IssueStatusFilter.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { Issue, Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const statuses: { label: string; value?: Status }[] = [
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+  { label: "Closed", value: "CLOSED" },
+];
+
+const IssueStatusFilter = () => {
+  const router = useRouter();
+
+  return (
+    <Select.Root
+      defaultValue=""
+      onValueChange={(status) => {
+        const query = status ? `?status=${status}` : "";
+        router.push("/issues" + query);
+      }}
+    >
+      <Select.Trigger />
+      <Select.Content>
+        {statuses.map((status) => (
+          <Select.Item key={status.label} value={status.value || ""}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssueStatusFilter;
