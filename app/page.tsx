81a29d7f556ed5a6ab384af02e014ece82a4f4f2
saurabhraj123 <me4saurabh4u@gmail.com>
2024-03-15 20:19:37 +0530

    Implement latest issues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
 export default function Home() {
-  return <Pagination currentPage={2} totalCount={100} pageSize={10} />;
+  return <LatestIssues />;
 }
